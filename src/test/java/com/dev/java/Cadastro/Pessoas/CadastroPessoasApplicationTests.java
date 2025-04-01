73db68c7a9453a8d457f604f08342f96a44b6d04
Luis Felipe R . L <luisramires29032002@gmail.com>
2025-04-01 10:57:16 -0300

    chore: Configurando  o ambiente

---
+++ src/test/java/com/dev/java/Cadastro/Pessoas/CadastroPessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.java.Cadastro.Pessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroPessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
