fb3a04a6c99e0d53c4a6489782385a1d33390c19
Luis Felipe R . L <luisramires29032002@gmail.com>
2025-04-03 16:11:12 -0300

    fix: coloca a coluna de email como unique

--- src/main/java/com/dev/java/Cadastro/Pessoas/Model/Missoes/MissoesModel.java
+++ src/main/java/com/dev/java/Cadastro/Pessoas/Model/Missoes/MissoesModel.java
@@ -2,11 +2,13 @@ package com.dev.java.Cadastro.Pessoas.Model.Missoes;
 
 import com.dev.java.Cadastro.Pessoas.Model.Pessoas.PessoaModel;
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@Data
 public class MissoesModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,37 +17,4 @@ public class MissoesModel {
     private  int dificuldade;
     @OneToMany(mappedBy = "missoes" )
     private List<PessoaModel> pessoas;
-
-    public MissoesModel() {
-    }
-
-    public MissoesModel(Long id, String nome, int dificuldade) {
-        this.id = id;
-        this.nome = nome;
-        this.dificuldade = dificuldade;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getDificuldade() {
-        return dificuldade;
-    }
-
-    public void setDificuldade(int dificuldade) {
-        this.dificuldade = dificuldade;
-    }
 }
--- src/main/java/com/dev/java/Cadastro/Pessoas/Model/Pessoas/PessoaModel.java
+++ src/main/java/com/dev/java/Cadastro/Pessoas/Model/Pessoas/PessoaModel.java
@@ -2,6 +2,7 @@ package com.dev.java.Cadastro.Pessoas.Model.Pessoas;
 
 import com.dev.java.Cadastro.Pessoas.Model.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.Data;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.List;
@@ -9,46 +10,16 @@ import java.util.UUID;
 
 @Entity
 @Table(name = "tb_cadastro")
+@Data
 public class PessoaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+    @Column(unique = true)
     private String email;
     private int idade;
     @ManyToOne()
     @JoinColumn(name = "missoes_id")
     private  MissoesModel missoes;
-    public  PessoaModel (){
-
-    }
-    public PessoaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
