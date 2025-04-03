627a836fe6faabd077230d7f695cf48da8afbad6
Luis Felipe R . L <luisramires29032002@gmail.com>
2025-04-03 10:30:28 -0300

    feat: Tracking Model Pessoa para o banco de dados

---
+++ src/main/java/com/dev/java/Cadastro/Pessoas/Model/PessoaModel.java
@@ -0,0 +1,50 @@
+package com.dev.java.Cadastro.Pessoas.Model;
+
+import jakarta.persistence.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_cadastro")
+public class PessoaModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+
+    public  PessoaModel (){
+
+    }
+    public PessoaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
